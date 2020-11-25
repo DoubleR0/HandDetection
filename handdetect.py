e158cffa680b4bd68a30335ec8a08a7fa85376ff
DoubleR0 <42911836+DoubleR0@users.noreply.github.com>
2020-11-26 02:38:10 +0700

    Add files via upload
    last version HandPresented

--- handdetect.py
+++ handdetect.py
@@ -1,58 +1,115 @@
+### Hand Presented ###
+
+# import module
 import cv2
-# import imutils
 import numpy as np
-import matplotlib.pyplot as plt
+import math
+import pyautogui
+import time
 
+# capture video
 cap0 = cv2.VideoCapture(0)
-# fgbg = cv2.bgsegm.createBackground
-# cap1 = cv2.VideoCapture(0)
-
-kernelD = np.ones((5, 5), np.uint8)
-kernelE = np.ones((3, 3), np.uint8)
+# set range color
 lower = np.array([0, 48, 80], dtype = "uint8")
 upper = np.array([30, 255, 255], dtype = "uint8")
-
-# lower = np.array([0, 80, 80], dtype = "uint8")
-# upper = np.array([30, 255, 255], dtype = "uint8")
-template = cv2.imread('./public/dataset/C_001.jpg', 0)
-
+# set time counter
+starttime = 0.0
 
 while(1):
+    # read frame
     _, frame0 = cap0.read()
-    edges = cv2.Canny(frame0, 100, 200)
-    # hsv0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-
-    # _, frame1 = cap1.read()
-    # hsv1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    # set size of ROI
+    roi=frame0[100:300, 100:300]
+    # set position of rectangle
+    cv2.rectangle(frame0,(100,100),(300,300),(0,255,0),2)
     
-
-    hsv = cv2.cvtColor(frame0, cv2.COLOR_BGR2HSV)
-    # gray = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
-    # dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-    
-    # dila = cv2.dilate(dst, None)
-    # frame0[ dst > 0.01*dst.max()] = [0,0,255]
+    # convert to hsv
+    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    # covert range of color to black and white
     mask = cv2.inRange(hsv, lower, upper)
-    dilation = cv2.dilate(mask, kernelD, iterations=2)
-    erode = cv2.erode(mask, kernelE, iterations=1)
+    # blur for delete noise
+    blurred = cv2.blur(mask, (5,5))
+    # set threshold hand
+    ret,thresh = cv2.threshold(blurred,0,255,cv2.THRESH_BINARY)
 
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    contours = max(contours, key=lambda x: cv2.contourArea(x))
+    # find contours
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    # if no contours
+    if(contours == []):
+        # show frame
+        cv2.imshow('frame0', frame0)
+        # wait key of escape
+        k = cv2.waitKey(33)
+        if k == 27:
+            break
+    # if has contours
+    else:
+        # set lange of contours area
+        contours = max(contours, key=lambda x: cv2.contourArea(x))
+        # draw contours on frame
+        cv2.drawContours(roi, [contours], -1, (255,255,0), 2)
 
-    hull = cv2.convexHull(contours)
-    defects = cv2.convexityDefects(contours[0], hull)
+        # return convexHull and find convexityDefects
+        hull = cv2.convexHull(contours, returnPoints=False)
+        defects = cv2.convexityDefects(contours, hull)
 
-    cv2.drawContours(frame0, [hull, contours], -1, (0,255,0), 2)
+        # set defects counter
+        count_defects = 0
+        # loop find defects
+        for i in range(defects.shape[0]):
+            # print("defects ===== ", defects[i, 0])
+            # set position of defects
+            s, e, f, d = defects[i, 0]
+            # separate start, end and far
+            start = tuple(contours[s][0])
+            end = tuple(contours[e][0])
+            far = tuple(contours[f][0])
 
+            # clculate triangle find angle less than 90
+            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+            angle = (math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 180) / 3.14
 
-    cv2.imshow('frame', frame0)
-    cv2.imshow('mask', mask)
-    cv2.imshow('dilation', dilation)
-    cv2.imshow('edges', edges)
-    # cv2.imshow('erode', erode)
-    # cv2.imshow('frame', frame0)
-    cv2.imshow('hsv', hsv)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+            # if angle < 90 draw a circle at the end point
+            if angle <= 90:
+                count_defects += 1
+                cv2.circle(roi, end, 10, [0, 0, 255], -1)
+            # draw convexHull in ROI
+            cv2.line(roi, start, end, [0, 255, 255], 2)
+
+        # action each number of defect
+        if count_defects == 1:
+            # set endtime
+            endtime = time.time()
+            if (endtime-starttime) > 2:
+                # action keybord event
+                pyautogui.press('right')
+                print("right")
+                # set new starttime
+                starttime = time.time()
+            else:
+                print("wait")
+        elif count_defects == 3:
+            # set endtime
+            endtime = time.time()
+            if (endtime-starttime) > 2:
+                # action keybord event
+                pyautogui.press('left')
+                print("left")
+                # set new starttime
+                starttime = time.time()
+            else:
+                print("wait")
+            #  time.sleep(2)
+        elif count_defects == 4:
+            # action keybord event
+            pyautogui.press('esc')
+
+        # show frame
+        cv2.imshow('frame0', frame0)
+        # wait key of escape
+        k = cv2.waitKey(33)
+        if k == 27:
+            break
 cv2.destroyAllWindows()
